4328b578556714eb46f59f673f7642d97e8c6b11
seo-yeongjun <zanygeek8371@gmail.com>
2022-07-07 03:24:10 +0900

    이분 탐색

---
+++ baekjoon/S10815_숫자카드.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S10815_숫자카드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Integer[] 보유 = new Integer[Integer.parseInt(br.readLine())];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 보유.length; i++)
+            보유[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(보유);
+        int 비교 = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 비교; i++) {
+            if (Arrays.binarySearch(보유, Integer.parseInt(st.nextToken())) >= 0)
+                bw.write("1 ");
+            else
+                bw.write("0 ");
+        }
+        br.close();
+        bw.close();
+    }
+}
