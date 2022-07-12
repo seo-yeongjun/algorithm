4198ea75ea87372e06e49e96d21e353a981b8d4c
seo-yeongjun <zanygeek8371@gmail.com>
2022-07-12 23:49:15 +0900

    기하학

---
+++ baekjoon/S3053_택시기하학.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/3053
+public class S3053_택시기하학 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(Math.PI*i*i);
+        System.out.println(i*i*2);
+    }
+}
